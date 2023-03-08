0229fd5854beeaec03b8c14dad2d47136655ff96
badr151 <badrkahouaji26@gmail.com>
2023-03-08 23:58:32 +0100

    apiservices en progress

---
+++ employee/src/Apiservices/employeelist.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const apiBase = "http://localhost:8080/Api/employees";
+
+const productServices = {
+  getAll() {
+    return fetch(apiBase)
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  getById(id) {
+    return axios.get(apiBase + `/${id}`).then((res) => res.data);
+  },
+  deleteById(id) {
+    return axios
+      .delete(apiBase + `/${id}`)
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+  },
+  create(data) {
+    return axios
+      .post(apiBase, data)
+      .then((res) => res.data)
+      .catch((err) => console.log(err));
+  },
+
+
+ editById(id, changes) {
+  return axios
+    .put(
+      `http://localhost:8080/Api/114f073f-1fa1-4289-98a6-f758f76c8bd0${id}`,
+      changes
+    )
+    .then((response) => response.data);
+},
+};
+
+export default employeelist;
---
+++ employee/src/Apiservices/employeeservices.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import employeelist from "./employeelist";
+
+const apiFavs =
+  "http://localhost:8080/Api/employees";
+
+const favoriteServices = {
+  getAll() {
+    return fetch(apiFavs)
+      .then((res) => res.json())
+      .then((data) => data);
+  },
+  getById(id) {
+    return axios.get(apiFavs + `/${id}`).then((res) => res.data);
+  },
+};
+
+export default employeelist;
