969f10c910e615ab972bcbce51a02ef2f36c94de
Aneebch <aneeb.chatha786@gmail.com>
2023-03-09 11:29:54 +0100

    Creating React Router

--- employee/src/layout/Navbar.js
+++ employee/src/layout/Navbar.js
@@ -1,27 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Employees
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Employees</a>
-    <button
-      className="navbar-toggler" 
-      type="button" 
-      data-bs-toggle="collapse" 
-      data-bs-target="#navbarSupportedContent" 
-      aria-controls="navbarSupportedContent" 
-      aria-expanded="false" 
-      aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <button className="btn btn-outline-light">Add New User</button>
-  </div>
-</nav>
-
+          <Link className="btn btn-outline-light" to="/adduser">
+            Add New User
+          </Link>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
