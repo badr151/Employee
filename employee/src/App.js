c8fc9089bc3d6c773f8945044723ec2b73cff8c6
Abdelhakim <abdelhakim8304@gmail.com>
2023-03-09 09:53:35 +0100

    create Navbar/Home using bootstrap

--- employee/src/App.js
+++ employee/src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
